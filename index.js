2ee5580612d9354a8481378c82ff812bd3e73c13
yoojin <yoojin9005@gmail.com>
2022-07-11 17:09:07 -0400

    undate04

--- index.js
+++ index.js
@@ -10,12 +10,25 @@ fetch('https://cost-of-living-and-prices.p.rapidapi.com/cities', {
     .then(travelData => {
         const cityArray = travelData.cities
         usInfo = cityArray.filter(countryObj => countryObj.country_name === 'United States')
-        
+        // console.log(usInfo)
+        document.getElementById('form').addEventListener('submit',(e)=> {
+            e.preventDefault()
+            const cityData = e.target.city.value
+            const stateData = e.target.state.value
+            
+            const filteredState = usInfo.filter(state => state.state_code === stateData)
+            const filteredCity = filteredState.filter(city => city.city_name === cityData)
+            console.log(filteredCity[0])
+
+            const divResult = document.querySelector('div')
+            const ulResult = document.createElement('ul')
+            const liResult = document.createElement('li')
+
+            ulResult.innerText = filteredState[0].state_code
+            liResult.innerText = filteredCity[0].city_name
+            divResult.append(ulResult)
+            ulResult.append(liResult)
+        })
+    
     })
 
-document.getElementById('form').addEventListener('submit',(e)=> {
-    e.preventDefault()
-    const cityData = e.target.city.value
-    const stateData = e.target.state.value
-    console.log(cityData,stateData)
-})
